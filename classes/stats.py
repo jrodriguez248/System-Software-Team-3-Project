2ef0148050d5d5a7c1a281eca5a1cb86d13904f0
BAIVY <148007827+BAIVY@users.noreply.github.com>
2024-05-10 05:30:49 -0400

    Create stats.py

---
+++ classes/stats.py
@@ -0,0 +1,13 @@
+'''
+Code meant to track stats and inventory of different entities
+'''
+
+# Player statistics for actions such as fighting
+class player_stats:
+    player_health = 100
+    player_hunger = 100
+    tool_sword = 1
+
+# Enemy statistics for fighting
+class enemy_stats:
+    enemy_health = 100
